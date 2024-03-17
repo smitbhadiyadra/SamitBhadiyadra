e2930ee40e0feac7f8fa584ba556d018720d9993
smitbhadiyadra <bhadiyadrasmit24@gmail.com>
2024-03-17 15:57:55 +0530

    all files upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function locoMotive(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, 
+    getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+locoMotive()
+
+
+tl = gsap.timeline()
+
+tl.from("nav>.navRight>a",{
+    opacity: 0,
+    stagger: .1,
+    duration: 1,
+    delay: .1
+},"a")
+
+tl.from(".page1>.hero>.slider-container",{
+    x: -100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.3
+},"a")
+
