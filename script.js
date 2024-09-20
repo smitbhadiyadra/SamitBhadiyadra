ed66d734424f15cd3720cfc853b9ddcbf8c50d3e
smitbhadiyadra <bhadiyadrasmit24@gmail.com>
2024-09-21 01:07:37 +0530

    Animation Changed

--- script.js
+++ script.js
@@ -27,10 +27,11 @@ locoMotive()
 
 tl = gsap.timeline()
 
-tl.from("nav>.navRight>a",{
+tl.from(".navRight a",{
     opacity: 0,
+    y: 100,
     stagger: .1,
-    duration: .5,
+    duration: 1,
     delay: .1
 },"a")
 
@@ -41,3 +42,37 @@ tl.from(".page1>.hero>.slider-container",{
     delay: 0.1
 },"a")
 
+tl.from(".page1 #text1",{
+    x: 100,
+    duration: 1,
+    delay: 0.2,
+    rotate: 3,
+},"a")
+tl.from(".hero #text2",{
+    x: -100,
+    duration: 1,
+    delay: 0.2,
+    rotate: 3,
+},"a")
+tl.from(".page1 #text3",{
+    x: 100,
+    duration: 1,
+    delay: 0.2,
+    rotate: 3,
+},"a")
+
+tl.from(".hero .links .box",{
+    opacity: 0,
+    y: 30,
+    scale: .5,
+    stagger: .1,
+    duration: 1,
+    delay: .4
+},"a")
+
+tl.from(".hero>span",{
+    opacity: 0,
+    y: 30,
+    duration: .6,
+    delay: 0.8
+},"a")
